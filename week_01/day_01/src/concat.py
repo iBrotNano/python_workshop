8f3b2d63d897cd3aff3dc14a4e749b62f07a55e9
iBrotNano <marcelmelzig@gmail.com>
2025-12-16 09:05:23 +0100

    Comments in concat.py

--- week_01/day_01/src/concat.py
+++ week_01/day_01/src/concat.py
@@ -1,9 +1,10 @@
-my_simple_concat = "Hallo" + " " + "Welt"
+my_simple_concat = "Hallo" + " " + "Welt" # Strings are concatenated into a single one.
 print(my_simple_concat)
 
-my_con_cat = "Katze" + " " + "Cat" + " " + "Kitten" + " " + "😺"
+my_con_cat = "Katze" + " " + "Cat" + " " + "Kitten" + " " + "😺" # Emojis works
 print(my_con_cat)
 
+# An example might be the generation of an email greeting and text.
 user_first_name = "Peter"
 user_last_name = "Musterfrau"
 user_age = 45
@@ -15,6 +16,7 @@ print(email_greeting)
 email_text = "Happy Birthday! You are now: " + str(user_age)
 print(email_text)
 
+# Another example could be a file path.
 my_path = "C:/user/me/images"
 my_file_base_name = "image"
 my_file_index = "001"
@@ -22,5 +24,5 @@ my_file_extension = ".png"
 full_file_path = my_path + "/" + my_file_base_name + my_file_index + my_file_extension
 print(full_file_path)
 
-test = 1 + int(float("1.0"))
+test = 1 + int(float("1.0")) # Datatypes can be casted into other ones.
 print(test)
